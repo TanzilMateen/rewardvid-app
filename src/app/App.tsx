3475834904f22980ea1c2b23c612471065e1c99d
TanzilMateen <mrtanzeellkjh@gmail.com>
2026-02-26 02:09:07 +0500

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import { router } from './routes'; // 👈 Check karein ye curly braces mein ho
 
 export default function App() {
   return <RouterProvider router={router} />;
