d3a305bb23d671d38cb70ed577b01346344150d5
TanzilMateen <mrtanzeellkjh@gmail.com>
2026-02-26 02:15:02 +0500

    Change import from react-router to react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes'; // 👈 Check karein ye curly braces mein ho
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes'; // 👈 Check karein ke file ka naam 'routes.tsx' hi hai
 
 export default function App() {
   return <RouterProvider router={router} />;
